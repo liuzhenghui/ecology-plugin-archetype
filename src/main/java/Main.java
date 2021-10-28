1c102f3d54c80ffa6108481839cdf8405c7b46fc
liuzhenghui <liu.zhenghui@outlook.com>
2021-10-28 22:32:59 +0800

    0.0.5

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("mvn archetype:generate -DgroupId=com.ecology -DartifactId=ecology-shizhengfu-maven -Dversion=1.0.0 -DarchetypeGroupId=com.github.liuzhenghui -DarchetypeArtifactId=ecology-plugin-archetype -DarchetypeVersion=0.0.5 -DinteractiveMode=false -DpackageName=shizhengfu -DpluginName=市政府项目二开");
+    }
+}
